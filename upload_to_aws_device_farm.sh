1d26390b37e502c8377ca0ecdb4e4d8b2192d5f6
Kamal Mukkamala <Kamalnrf@yahoo.com>
2019-07-01 17:31:43 +0530

    change debug apk to release apk

--- upload_to_aws_device_farm.sh
+++ upload_to_aws_device_farm.sh
@@ -7,4 +7,4 @@ echo "=============CREATE UPLOAD======================"
 preSignedUrl=`aws devicefarm create-upload --project-arn $(echo $projectArn) --name app-debug.apk --type ANDROID_APP | jq '.upload.url'`
 
 echo "=============UPLOAD APK========================="
-curl -T app/build/outputs/apk/debug/app-debug.apk "$(echo $preSignedUrl | sed -e 's/^"//' -e 's/"$//')"
+curl -T app/build/outputs/apk/release/app-release-unsigned.apk "$(echo $preSignedUrl | sed -e 's/^"//' -e 's/"$//')"
