bdacc8894fd6b8b045ab27e997a5088bed2f12c6
Kamal Mukkamala <kamalnrf@yahoo.com>
2019-07-29 10:46:26 +0530

    Upload test apk to aws

--- upload_to_aws_device_farm.sh
+++ upload_to_aws_device_farm.sh
@@ -4,7 +4,9 @@ echo "=============GET PROJECT ARN===================="
 projectArn=`aws devicefarm list-projects | jq '.projects[0].arn' | sed -e 's/^"//' -e 's/"$//'`
 
 echo "=============CREATE UPLOAD======================"
-preSignedUrl=`aws devicefarm create-upload --project-arn $(echo $projectArn) --name app-debug.apk --type ANDROID_APP | jq '.upload.url'`
+appPreSignedUrl=`aws devicefarm create-upload --project-arn $(echo $projectArn) --name app-debug.apk --type ANDROID_APP | jq '.upload.url'`
+testAppPreSignedUrl=`aws devicefarm create-upload --project-arn $(echo $projectArn) --name app-debug.apk --type INSTRUMENTATION_TEST_PACKAGE | jq '.upload.url'`
 
 echo "=============UPLOAD APK========================="
 curl -T app/build/outputs/apk/debug/app-debug.apk "$(echo $preSignedUrl | sed -e 's/^"//' -e 's/"$//')"
+curl -T app/build/outputs/apk/androidTest/debug/app-debug-androidTest.apk
