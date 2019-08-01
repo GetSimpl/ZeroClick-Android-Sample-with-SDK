9ff53677cd1cdf30fd3610699488b955ebdd5d9a
Kamal Mukkamala <kamalnrf@yahoo.com>
2019-08-01 14:24:38 +0530

    Update the scripts to upload the test files aswell

--- upload_to_aws_device_farm.sh
+++ upload_to_aws_device_farm.sh
@@ -1,12 +1,64 @@
 #!/usr/bin/env bash
 
-echo "=============GET PROJECT ARN===================="
-projectArn=`aws devicefarm list-projects | jq '.projects[0].arn' | sed -e 's/^"//' -e 's/"$//'`
+get_arn () {
+    data=$1
+    arnName=$2
+    local arn=""
+    counter=0
 
-echo "=============CREATE UPLOAD======================"
-appPreSignedUrl=`aws devicefarm create-upload --project-arn $(echo $projectArn) --name app-debug.apk --type ANDROID_APP | jq '.upload.url'`
-testAppPreSignedUrl=`aws devicefarm create-upload --project-arn $(echo $projectArn) --name app-debug-android-Test.apk --type INSTRUMENTATION_TEST_PACKAGE | jq '.upload.url'`
+    while [ -z $arn ]; do
+        name=`echo $data | jq ".[$counter].name"`
 
-echo "=============UPLOAD APK========================="
-curl -T app/build/outputs/apk/debug/app-debug.apk "$(echo $preSignedUrl | sed -e 's/^"//' -e 's/"$//')"
-curl -T app/build/outputs/apk/androidTest/debug/app-debug-androidTest.apk
+        [[ $name == "\"$arnName\"" ]] && arn=`echo $data | jq ".[$counter].arn" | sed -e 's/^"//' -e 's/"$//'`
+        [[ $name == null ]] && exit 1 # Exits if the pool name is not there in the list
+
+        counter=`expr $counter + 1`
+    done
+
+    echo $arn
+}
+
+echo "============= GET PROJECT ARN ===================="
+projects=`aws devicefarm list-projects | jq '.projects'`
+projectArn=`get_arn "$projects" "SDK CI Tests"`
+
+echo "============= GET DEVICE POOL ==================="
+devicePools=`aws devicefarm list-device-pools --arn $projectArn | jq .devicePools`
+devicePoolArn=`get_arn "$devicePools" "SDK Release Testing"`
+
+echo "============= CREATE UPLOAD ======================"
+appUpload=`aws devicefarm create-upload --project-arn $(echo $projectArn) --name app-debug.apk --type ANDROID_APP | jq '.upload'`
+testAppUpload=`aws devicefarm create-upload --project-arn $(echo $projectArn) --name app-debug-android-Test.apk --type INSTRUMENTATION_TEST_PACKAGE | jq '.upload'`
+
+appPreSignedUrl=`echo $appUpload | jq '.url'`
+appUploadArn=`echo $appUpload | jq '.arn' | sed -e 's/^"//' -e 's/"$//'`
+testAppPreSignedUrl=`echo $testAppUpload | jq '.url'`
+testAppUploadArn=`echo $testAppUpload | jq '.arn'  | sed -e 's/^"//' -e 's/"$//'`
+
+echo "============= UPLOAD APK ========================="
+curl -T app/build/outputs/apk/debug/app-debug.apk "$(echo $appPreSignedUrl | sed -e 's/^"//' -e 's/"$//')"
+curl -T app/build/outputs/apk/androidTest/debug/app-debug-androidTest.apk "$(echo $testAppPreSignedUrl | sed -e 's/^"//' -e 's/"$//')"
+
+echo "============= RUN TESTS =========================="
+runArn=`aws devicefarm schedule-run --project-arn $projectArn --app-arn $appUploadArn --device-pool-arn $devicePoolArn --name app-debug.apk --test type=INSTRUMENTATION,testPackageArn=$testAppUploadArn | jq '.run.arn'  | sed -e 's/^"//' -e 's/"$//`
+
+echo $runArn
+status=false
+
+while [ $status==false ]; do
+    result=`aws devicefarm get-run --arn $runArn | jq '.run`
+    echo $result
+    if [ `echo $result | jq '.status'`=="COMPLETED" ]
+    then
+        if [ `echo $result | jq '.result` != "SUCCESS"]
+        then
+            echo "============= TESTS HAVE FAILED =============="
+            exit 1
+        fi
+    else
+        sleep 2m
+    fi
+done
+
+echo "============= TESTS HAVE PASSED ================="
+exit 0
